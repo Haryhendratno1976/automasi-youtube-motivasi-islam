7b1b94fabbafc6e17cfee19827b06e4e87810965
Haryhendratno1976 <haryhendratno86@gmail.com>
2026-09-01 22:09:28 +0700

    Update pronunciation adjustments for specific terms

--- modules/creator.py
+++ modules/creator.py
@@ -599,8 +599,8 @@ class VideoCreator:
             # ==============================================================
             # TAMBAHKAN KODE PENYESUAIAN PELAFALAN DI SINI
             # ==============================================================
-            text = text.replace("Allah", "Alloh").replace("Rasulullah", "Rosululloh")
-            text = text.replace("allah", "alloh").replace("rasulullah", "rosululloh")
+            text = text.replace("Allah", "Owlloh").replace("Rasulullah", "Rosululloh")
+            text = text.replace("allah", "owlloh").replace("rasulullah", "rosululloh")
             # ==============================================================
             seg_path = self.temp_dir / f"voiceover_{language}_seg{i}.mp3"
 
